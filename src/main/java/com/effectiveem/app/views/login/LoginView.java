0e9924d3dbef52ba0318a907fc25c7de3c5abdb0
simeshev <imeshev@gmail.com>
2023-06-14 13:32:59 -0700

    Expanded the header label.

--- src/main/java/com/effectiveem/app/views/login/LoginView.java
+++ src/main/java/com/effectiveem/app/views/login/LoginView.java
@@ -24,7 +24,7 @@ public class LoginView extends LoginOverlay implements BeforeEnterObserver {
 
         LoginI18n i18n = LoginI18n.createDefault();
         i18n.setHeader(new LoginI18n.Header());
-        i18n.getHeader().setTitle("Effective EM");
+        i18n.getHeader().setTitle("Effective Engineering Manager Tools");
         i18n.getHeader().setDescription("Login using user/user or admin/admin");
         i18n.setAdditionalInformation(null);
         setI18n(i18n);
